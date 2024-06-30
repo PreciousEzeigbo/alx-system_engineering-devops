6f5edd387f0def7513b81814f3a6f5020274af5d
Precious Ezeigbo <preciousezeigbo81@gmail.com>
2024-06-30 10:19:05 -0700

    Fix error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,6 @@
-# 1-install_a_package.pp
+# Install flask from pip3
 
-# Ensure pip3 is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Ensure Flask version 2.1.0 is installed
-exec { 'install flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+exec { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
