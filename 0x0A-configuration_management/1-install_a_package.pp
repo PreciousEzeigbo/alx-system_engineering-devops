7027ab362e0b50859a90e56caee6de87f2db8ae5
Precious Ezeigbo <preciousezeigbo81@gmail.com>
2024-06-30 10:00:47 -0700

    Fixing error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,21 +1,6 @@
-# Install a package using Puppet
-
-# Install python3.8
-package { 'python3.8':
-    ensure   => '3.8.10',
-    provider => 'pip3',
-}
-
 # Install Flask using pip3
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
-}
-
-# Install Werkzeug with a dependency on Flask
-package { 'Werkzeug':
-    ensure   => '2.1.1',
-    provider => 'pip3',
-    require  => Package['Flask'],
 }
 
